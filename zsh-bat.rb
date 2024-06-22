3f275f4ef418a90902e0738cd006040f191a41aa
Philip Gaitan <gojira@Philips-MacBook-Air.local>
2024-06-22 00:02:35 -0500

    Bugfix

--- zsh-bat.rb
+++ zsh-bat.rb
@@ -1,8 +1,8 @@
 class ZshBat < Formula
     desc "Use 'bat' as a syntax highlighter in Zsh"
     homepage "https://github.com/fdellwing/zsh-bat"
-    url "https://github.com/fdellwing/zsh-bat/archive/refs/tags/v0.5.tar.gz"
-    sha256 "d85814dc9d1b11a4f72b8f46dbe9f084c588dd3c3f33ee5b3502b1e3babe237b"
+        url "https://github.com/fdellwing/zsh-bat"
+        sha256 "d85814dc9d1b11a4f72b8f46dbe9f084c588dd3c3f33ee5b3502b1e3babe237b"
     license "MIT"
   
     depends_on "bat"
